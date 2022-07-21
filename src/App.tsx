b3b11b9e3644de4ab4e5974a22cae00efc56eae3
Niamh <niamh.brockbank@gmail.com>
2022-07-21 16:58:20 +0100

    Sort alphabetically names

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,33 @@
-import { greet } from "./utils/greet";
 import babyNames from "./babyNamesData.json";
 
+// function alphCompare(a: string, b:string){
+//   if (a < b) {
+//     return -1;
+//   }
+//   if (a > b) {
+//     return 1;
+//   }
+//   // a must be equal to b
+//   return 0;
+// }
+
 function App(): JSX.Element {
-  const nameList: JSX.Element[] = []
+  const nameList: string[] = []
   for (const baby of babyNames){
-    nameList.push(<li>{baby.name}</li>)
+    nameList.push(baby.name)
   }
+
+  nameList.sort()
+  const nameListElements : JSX.Element[] = []
+  for (const name of nameList){
+    nameListElements.push(<li>{name}</li>)
+  }
+
   return (
     <>   
     <ul>
-      {nameList}
+      {nameListElements}
     </ul>
-    <h1>{greet("World")}</h1>; 
     </>
 
   )
