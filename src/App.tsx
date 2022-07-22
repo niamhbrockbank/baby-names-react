23acffe1f8b53e511427c918c2328b2dade9e8c9
Niamh <niamh.brockbank@gmail.com>
2022-07-22 12:44:26 +0100

    Sort names alphabetically

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,18 @@ interface BabyName {
   sex : string //ideally set to m or f
 }
 const convertToElement = (n : BabyName) => <li key={n.id} className={n.sex}>{n.name}</li>
+const compareAlph = (a: BabyName, b: BabyName) => {
+  if ( a.name < b.name ){
+    return -1;
+  }
+  if ( a.name > b.name ){
+    return 1;
+  }
+  return 0;
+}
 
 function App(): JSX.Element {
+  babyNames.sort(compareAlph)
   const nameList: JSX.Element[] = [];
   for (const baby of babyNames) {
     nameList.push(convertToElement(baby));
