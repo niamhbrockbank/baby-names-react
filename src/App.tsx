c0aa3cd14ebeffc0f5037a472d529bafdf3145c4
Niamh <niamh.brockbank@gmail.com>
2022-07-22 14:19:43 +0100

    Move name list to own component file

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,12 @@
-import babyNames from "./babyNamesData.json";
 import "./styles.css";
-
-interface BabyName {
-  id: number;
-  name: string;
-  sex: string; //ideally set to m or f
-}
-const convertToElement = (n: BabyName) => (
-  <li key={n.id} className={n.sex}>
-    {n.name}
-  </li>
-);
-const compareAlph = (a: BabyName, b: BabyName) => {
-  if (a.name < b.name) {
-    return -1;
-  }
-  if (a.name > b.name) {
-    return 1;
-  }
-  return 0;
-};
+import NameSection from "./components/NameSection";
 
 function App(): JSX.Element {
-  babyNames.sort(compareAlph);
-  const nameList: JSX.Element[] = [];
-  for (const baby of babyNames) {
-    nameList.push(convertToElement(baby));
-  }
 
   return (
     <>
       <link rel="stylesheet" href="../styles.css"></link>
-      <ul id="name-list">{nameList}</ul>
+      <NameSection />
     </>
   );
 }
